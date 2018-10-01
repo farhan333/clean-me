5261599b80c0e8efd0f51e21ec0c55951ffac6bb
Farhan - Pantheon <farhan@fixstars.com>
2018-10-01 13:30:59 -0700

    The Script

---
+++ clean-me.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt update
+apt update
+apt autoclean
+apt autoclean
+apt autoremove
+apt autoremove
+apt autoremove --purge
+apt autoremove --purge
+apt dist-upgrade
+apt dist-upgrade
+
+
